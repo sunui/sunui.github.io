1da16ce56fd703d6a4fb3bcfb478f5bfc2f61a76
sunxinlei <sunxinly@gmail.com>
2017-03-06 03:09:38 +0800

    Site updated: 2017-03-06 03:09:38

--- demo/resume/js/my.js
+++ demo/resume/js/my.js
@@ -1,18 +1,28 @@
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+    // 点击导航滑动效果
+    $('a[href*=#]:not([href=#])').click(function() {
+      console.log(this,this.hash);
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+    // 输入效果
+    $("#typed").typed({
+        strings: [
+            "JavaScript", "React", "HTML5/CSS3"
+        ],
+        typeSpeed: 180,
+        backDelay: 600,
+        contentType: "html",
+        loop: !0
+    })
 });
-function newTyped(){}$(function(){$("#typed").typed({
-    strings: ["呼伦贝尔","山西大学","东华光普大数据" ],
-
-    typeSpeed:180,backDelay:600,contentType:"html",loop:!0,resetCallback:function(){newTyped()}}),$(".reset").click(function(){$("#typed").typed("reset")})});
